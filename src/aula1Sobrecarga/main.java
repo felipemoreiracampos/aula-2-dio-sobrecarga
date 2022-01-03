3da97b8a4525748e9b2000deb76a32a072eeaf67
fcampos <felipemoreiracp@hotmail.com>
2022-01-03 09:59:54 -0300

    Class2 sobrecarga

---
+++ src/aula1Sobrecarga/main.java
@@ -0,0 +1,15 @@
+package aula1Sobrecarga;
+
+public class main {
+    public static void main(String[] args) {
+
+    // Quadrilátero
+    System.out.println("Exercício quadrilátero");
+    Quadrilatero.area(3);
+    Quadrilatero.area(5d,5d);
+    Quadrilatero.area(7,8,9);
+    Quadrilatero.area(5f,5f);
+
+}
+}
+
